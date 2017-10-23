58b07914d7bcca0df6aa7622836d39396ded44e0
Kevin Dennis <kevindennis@mail.usf.edu>
2017-10-23 14:30:36 -0400

    Re-ordered posts

--- articles.php
+++ articles.php
@@ -1,20 +1,20 @@
 <?php
 $articles = [
   [
-    "title" => "Shia Labeouf-off!",
-    "subtitle" => "Web Challenge, CSAW 2017",
-    "author" => "Kevin Dennis",
-    "date" => "October 16th, 2017",
-    "file" => "./md/csaw_2017_shia.md",
-    "downloads" => []
-  ],
-  [
     "title" => "Hidden Program (Warmup)",
     "subtitle" => "Exploitation Challenge, Pwn2win 2017",
     "author" => "Kevin Dennis",
     "date" => "October 23rd, 2017",
     "file" => "./md/pwn2win_2017_hidden.md",
     "downloads" => ["Hidden Program C Code" => "pwn2win_2017_hidden.c"]
+  ],
+  [
+    "title" => "Shia Labeouf-off!",
+    "subtitle" => "Web Challenge, CSAW 2017",
+    "author" => "Kevin Dennis",
+    "date" => "October 16th, 2017",
+    "file" => "./md/csaw_2017_shia.md",
+    "downloads" => []
   ],
 ];
 
